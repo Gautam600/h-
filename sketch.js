9db1f42f37612605ff5e0a58ec7bb0a32bd2afb8
gautam <bhavik3698@gmail.com>
2020-11-03 16:46:51 +0530

    read

--- sketch.js
+++ sketch.js
@@ -7,9 +7,15 @@ var distance = 0;
 var database;
 
 var form, player, game;
-
+var car1image,car2image,car3image,car4image,trackimage;
 var cars, car1, car2, car3, car4;
-
+function preload(){
+car1image=loadImage("images/car1.png");
+car2image=loadImage("images/car2.png");
+car3image=loadImage("images/car3.png");
+car4image=loadImage("images/car4.png");
+trackimage=loadImage("images/track.jpg");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +33,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
